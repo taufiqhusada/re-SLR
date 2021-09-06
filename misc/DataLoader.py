7e70411ee5dbfe13ed4b9fb93f32780eb520d73e
Taufiq Husada Daryanto <45747001+taufiqhusada@users.noreply.github.com>
2021-09-06 07:31:56 +0700

    split train test val

--- misc/DataLoader.py
+++ misc/DataLoader.py
@@ -71,17 +71,31 @@ class DataLoader:
         self.split_ix = {}
         self.iterators = {}
         cnt_ref_data = 0
-        for ref_id in self.Refs:
-            split = self.Refs[ref_id]['split']
-            if split not in self.split_ix:
-                self.split_ix[split] = []
-                self.iterators[split] = 0
-            if (split=='train'):
-                if (cnt_ref_data < 40):     # load small fraction of the train data
-                    self.split_ix[split].append(ref_id) 
-                    cnt_ref_data += 1
-            else:
-                self.split_ix[split].append(ref_id)
+        
+        list_split = ['train', 'test', 'val']
+        for split in list_split:
+            for ref_id in self.getRefIds(split=split)[:]:
+                if split not in self.split_ix:
+                    self.split_ix[split] = []
+                    self.iterators[split] = 0
+                if (split=='train'):
+                    if (cnt_ref_data < 40):     # load small fraction of the train data
+                        self.split_ix[split].append(ref_id) 
+                        cnt_ref_data += 1
+                else:
+                    self.split_ix[split].append(ref_id)
+        
+#         for ref_id in self.Refs:
+#             split = self.Refs[ref_id]['split']
+#             if split not in self.split_ix:
+#                 self.split_ix[split] = []
+#                 self.iterators[split] = 0
+#             if (split=='train'):
+#                 if (cnt_ref_data < 40):     # load small fraction of the train data
+#                     self.split_ix[split].append(ref_id) 
+#                     cnt_ref_data += 1
+#             else:
+#                 self.split_ix[split].append(ref_id)
             
         for split in self.split_ix:
             print('assigned {} refs to split {}'.format(len(self.split_ix[split]), split))
